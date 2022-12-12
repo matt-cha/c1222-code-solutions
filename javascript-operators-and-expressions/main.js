37e8995c37c8f237b1025e87b58256ce8291beda
Matt Cha <mcha002@ucr.edu>
2022-12-12 19:25:37 +0000

    javascript-operators-and-expressions - Matt Cha

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 2;
+var height = 3;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 5;
+var payment = 6;
+var change = payment - bill;
+
+console.log('value of payment:', payment);
+console.log('typeof change:', typeof change);
+
+var quizzes = 2;
+var midterm = 3;
+var final = 4;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Matt';
+var lastName = 'Cha';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+
+console.log('valueof isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 9001;
+var isSparta = headCount === 300;
+
+console.log('valueof isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+
+console.log('valueof motto:', motto);
+console.log('typeof motto:', typeof motto);
