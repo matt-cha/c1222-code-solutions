56453fd05a8758cfa111548b2aa64c0813e7b1f3
Matt Cha <mcha002@ucr.edu>
2022-12-13 18:16:57 +0000

    javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,50 @@
+var orderHistory = [
+  {
+    item: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    deliveredTo: null,
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'August 8, 2020',
+    returnWindowClose: 'September 7, 2020',
+    total: '$34.00',
+    price: '$31.55',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232'
+  },
+  {
+    item: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    deliveredTo: 'Resident',
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'July 20, 2020',
+    returnWindowClose: 'August 19, 2020',
+    total: '$44.53',
+    price: '$41.33',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257'
+  },
+  {
+    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wil U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
+    author: null,
+    deliveredTo: 'Resident',
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'July 7, 2020',
+    returnWindowClose: 'August 5, 2020',
+    total: '$17.22',
+    price: '$15.98',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409'
+  },
+  {
+    item: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sq'],
+    author: [null, 'Faroult, Stephane'],
+    deliveredTo: null,
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'July 5, 2020',
+    returnWindowClose: 'August 4, 2020',
+    total: '$138.93',
+    price: ['$94.95', '$33.99'],
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248'
+  }
+];
