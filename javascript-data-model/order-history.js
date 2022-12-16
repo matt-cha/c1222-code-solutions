3d7c6c84bd8b5d0594f8d1c0b8b78fa73d36b59f
Matt Cha <mcha002@ucr.edu>
2022-12-16 02:24:37 +0000

    javascript-data-model - data model change reformatting

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,50 +1,76 @@
 var orderHistory = [
   {
-    item: 'JavaScript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    deliveredTo: null,
+    orderNumber: '114-3941689-8772232',
     orderPlaced: 'August 4, 2020',
     orderDelivered: 'August 8, 2020',
     returnWindowClose: 'September 7, 2020',
+    deliveredTo: null,
     total: '$34.00',
-    price: '$31.55',
     shipTo: 'JS Masher',
-    orderNumber: '114-3941689-8772232'
+    items: [
+      {
+        type: 'book',
+        name: 'JavaScript for impatient programmer author:',
+        author: 'Rauschmayer, Dr. Axel',
+        price: '$31.55'
+      }
+    ]
   },
   {
-    item: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    deliveredTo: 'Resident',
+    orderNumber: '113-9984268-1280257',
     orderPlaced: 'July 19, 2020',
     orderDelivered: 'July 20, 2020',
     returnWindowClose: 'August 19, 2020',
+    deliveredTo: 'Resident',
     total: '$44.53',
-    price: '$41.33',
     shipTo: 'JS Masher',
-    orderNumber: '113-9984268-1280257'
+    items: [
+      {
+        type: 'book',
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        price: '$41.33'
+      }
+    ]
   },
   {
-    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wil U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
-    author: null,
-    deliveredTo: 'Resident',
+    orderNumber: '114-2875557-9059409',
     orderPlaced: 'July 4, 2020',
     orderDelivered: 'July 7, 2020',
     returnWindowClose: 'August 5, 2020',
+    deliveredTo: 'Resident',
     total: '$17.22',
-    price: '$15.98',
     shipTo: 'JS Masher',
-    orderNumber: '114-2875557-9059409'
+    items: [
+      {
+        type: 'electronic',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wil U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
+        author: null,
+        price: '$15.98'
+      }
+    ]
   },
   {
-    item: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sq'],
-    author: [null, 'Faroult, Stephane'],
-    deliveredTo: null,
+    orderNumber: '113-2883177-2648248',
     orderPlaced: 'July 3, 2020',
     orderDelivered: 'July 5, 2020',
     returnWindowClose: 'August 4, 2020',
+    deliveredTo: null,
     total: '$138.93',
-    price: ['$94.95', '$33.99'],
     shipTo: 'JS Masher',
-    orderNumber: '113-2883177-2648248'
+    items: [
+      {
+        type: 'electronic',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        price: '$94.95'
+      },
+      {
+        type: 'book',
+        name: 'The Art of Sq',
+        author: 'Faroult, Stephane',
+        price: '$33.99'
+      }
+    ]
   }
 ];
