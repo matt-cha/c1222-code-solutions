0b250c9ce280aa242012a7507a6e0590107ad5af
Matt Cha <mcha002@ucr.edu>
2022-12-19 21:56:38 +0000

    string-manipulation-2 4

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -15,9 +15,10 @@
  */
 
 function capitalizeWords(string) {
-  var newString = '';
+  var newString = string.split(' ');
   for (var i = 0; i < string.length; i++) {
-    newString = string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+    newString += string[0].toUpperCase() + string.slice(1).toLowerCase();
   }
   return newString;
 }
