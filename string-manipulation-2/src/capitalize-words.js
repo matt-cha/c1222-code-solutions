89ae9358dae0a58eba4017c0aec473849ff348f9
Matt Cha <mcha002@ucr.edu>
2022-12-19 18:37:30 +0000

    string-manipulation-2 commit 2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,23 @@
 /* exported capitalizeWords */
+/* -create a function that takes 1 argument, string
+-create an empty string and assign it to the variable newString
+-create a for loop with the intialization of i = 0
+-condition of i less than the string length
+-final expression of incrementing i by one each iteration
+-use the toUpperCase method of the string object  */
+/* function capitalizeWords(string) {
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    newString = string[0].toUpperCase() + string.slice(1).toLowerCase();
+  }
+  return newString;
+}
+ */
+
+function capitalizeWords(string) {
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    newString = string[0].toUpperCase() + string.slice(1).toLowerCase();
+  }
+  return newString;
+}
