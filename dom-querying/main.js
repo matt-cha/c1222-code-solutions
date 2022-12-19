1fb67dfc4767e6594fc8ce085ea0e9553cb3cb01
Matt Cha <mcha002@ucr.edu>
2022-12-19 17:44:27 +0000

    dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var h1 = document.querySelector('h1');
+console.log('heading 1:', h1);
+console.dir(h1);
+
+var explanation = document.querySelector('#explanation');
+console.log('explanation id:', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('hint class:', hint);
+console.dir(hint);
+
+var p = document.querySelectorAll('p');
+console.log('p element:', p);
+
+var examplelink = document.querySelectorAll('.example-link');
+console.log('example-link class:', examplelink);
