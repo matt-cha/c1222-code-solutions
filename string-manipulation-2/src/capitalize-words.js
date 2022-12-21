02edd30d2d10db2bf106b3f7d50065e8c5a1af20
Matt Cha <mcha002@ucr.edu>
2022-12-21 00:50:19 +0000

    string-manipulation-2 4 done

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,24 +1,22 @@
 /* exported capitalizeWords */
 /* -create a function that takes 1 argument, string
--create an empty string and assign it to the variable newString
+-call the split method of the string object with one argument, a space and assign it to the variable wordArray
 -create a for loop with the intialization of i = 0
--condition of i less than the string length
+-condition of i less than the wordArray length
 -final expression of incrementing i by one each iteration
--use the toUpperCase method of the string object  */
-/* function capitalizeWords(string) {
-  var newString = '';
-  for (var i = 0; i < string.length; i++) {
-    newString = string[0].toUpperCase() + string.slice(1).toLowerCase();
-  }
-  return newString;
-}
- */
+-uppercase the first character at index 0 of the word in the array at index i and add it to the lowercased
+rest of the word (starting index 1) at index i with the slice method to join them
+-assign the result of that expression to the wordArray at the index of i to reassign
+the string at only that indx of the array
+-combine all items in the array with the join method using an argument of a space
+and reassign the value to a new variable
+return that variable from the function */
 
 function capitalizeWords(string) {
-
-  var newString = string.split(' ');
-  for (var i = 0; i < newString.length; i++) {
-    newString = newString[i].toUpperCase() + newString.slice(i + 1).toLowerCase();
+  var wordArray = string.split(' ');
+  for (var i = 0; i < wordArray.length; i++) {
+    wordArray[i] = wordArray[i][0].toUpperCase() + wordArray[i].slice(1).toLowerCase();
   }
-  return newString;
+  var capitalizedString = wordArray.join(' ');
+  return capitalizedString;
 }
