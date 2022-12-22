b99ceaf73e62e929ddc47100bba6c34a8bfd525b
Matt Cha <mcha002@ucr.edu>
2022-12-22 18:46:39 +0000

    javascript-and-json

---
+++ javascript-and-json/main.js
@@ -0,0 +1,24 @@
+var library = [
+  {
+    isbn: '100',
+    title: '1984',
+    author: 'George Orwell'
+  },
+  {
+    isbn: '9',
+    title: 'A cooking book',
+    author: 'Betty Crocker'
+  },
+  {
+    isbn: '51',
+    title: 'The Schwa Was Here',
+    author: 'Andy Something'
+  }
+];
+console.log('type of library:', typeof library);
+var libraryJSON = JSON.stringify(library);
+console.log('type of JSON stringify library:', typeof libraryJSON);
+var jsonString = JSON.stringify({ name: 'Joe Mama', id: 8 });
+console.log('type of json string:', typeof jsonString);
+var jsonParse = JSON.parse(jsonString);
+console.log('type of json parse:', typeof jsonParse);
