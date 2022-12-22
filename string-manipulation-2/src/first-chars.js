6124e9351dfa21bf961d63af28236400d16ac78b
Matt Cha <mcha002@ucr.edu>
2022-12-22 16:54:12 +0000

    string-manipulation-2 1-7done

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,2 +1,22 @@
 /* exported firstChars */
-/*  */
+/* -create a function with 2 arguments
+-create an empty string
+create a new variable falsy
+-create a for loop that will return the string at length number
+-the string at index of i (character at that iteration) will be converted to a boolean
+-falsy is assigned that value and create an if statement for
+that variable falsy being strictly equal to true.
+-because all values are truthy unless they are falsy,
+only the truthy values are concatenated into the finalString at its i index.
+-loop will end when the i is equal to the length argument passed */
+function firstChars(length, string) {
+  var falsy;
+  var finalString = '';
+  for (var i = 0; i < length; i++) {
+    falsy = Boolean(string[i]);
+    if (falsy === true) {
+      finalString += string[i];
+    }
+  }
+  return finalString;
+}
