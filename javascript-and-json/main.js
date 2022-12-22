622e5a1845f7e59c15a47fef2fdb6ff720bed223
Matt Cha <mcha002@ucr.edu>
2022-12-22 22:00:22 +0000

    javascript-and-json fix JSON string

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -18,7 +18,7 @@ var library = [
 console.log('type of library:', typeof library);
 var libraryJSON = JSON.stringify(library);
 console.log('type of JSON stringify library:', typeof libraryJSON);
-var jsonString = JSON.stringify({ name: 'Joe Mama', id: 8 });
-console.log('type of json string:', typeof jsonString);
-var jsonParse = JSON.parse(jsonString);
+var JSONstring = '{"name": "Joe Mama", "id": 8}';
+console.log('type of json string:', typeof JSONstring);
+var jsonParse = JSON.parse(JSONstring);
 console.log('type of json parse:', typeof jsonParse);
