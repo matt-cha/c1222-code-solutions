de02641fdc5e81861fa04baf79fbdbce1ca31aed
Matt Cha <mcha002@ucr.edu>
2022-12-22 17:47:02 +0000

    string-manipulation-2 8 work

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,24 @@
 /* exported swapChars */
+/* -create a function that takes 3 arguments
+-create a new empty string
+-create a for loop that returns string secondIndex and
+firstIndex place and vice versa
+-return rest of the string as normal
+ */
+/* function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  for (var i = 0; i < string.length; i++) {
+    swappedCharacterString = string[secondIndex][firstIndex] + string[firstIndex][secondIndex];
+  }
+  return swappedCharacterString;
+} */
+function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  for (var i = 0; i < string.length; i++) {
+    string[firstIndex] = string[secondIndex];
+    /*     console.log('value of string[firstIndex]', string[firstIndex]); */
+    /*     string[secondIndex] = string.firstIndex; */
+    swappedCharacterString += string[i];
+  }
+  return swappedCharacterString;
+}
