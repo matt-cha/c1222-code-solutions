44d2db2a6151cd8d7ddcf91c8a32b8874ea129d2
Matt Cha <mcha002@ucr.edu>
2022-12-23 03:56:34 +0000

    8 still being worked

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,8 @@
 /* exported swapChars */
 /* -create a function that takes 3 arguments
 -create a new empty string
--create a for loop that returns string secondIndex and
-firstIndex place and vice versa
+-create a for loop that returns string secondIndex at the place of firstIndex
+and vice versa. to return secondIndex at the firstIndex,
 -return rest of the string as normal
  */
 /* function swapChars(firstIndex, secondIndex, string) {
@@ -22,14 +22,55 @@ firstIndex place and vice versa
   }
   return swappedCharacterString;
 } */
-function swapChars(firstIndex, secondIndex, string) {
+/* function swapChars(firstIndex, secondIndex, string) {
   var swappedCharacterString = '';
   var stringArray = string.split('');
-
+  console.log(stringArray);
   for (var i = 0; i < stringArray.length; i++) {
+
     stringArray[firstIndex] = stringArray.secondIndex;
     stringArray[secondIndex] = stringArray.firstIndex;
     swappedCharacterString = stringArray.join('');
   }
+  return swappedCharacterString;
+} */
+/* function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  var stringArray = string.split('');
+  var newSecondIndex = stringArray[secondIndex];
+  console.log('stringArray:', stringArray);
+  stringArray[firstIndex] = stringArray[secondIndex];
+  console.log('stringArray[firstIndex]:', stringArray[firstIndex]);
+  console.log('newSecondIndex:', newSecondIndex);
+} */
+/* function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  var stringArray = string.split('');
+  var newSecondIndex = stringArray[secondIndex];
+  console.log('stringArray:', stringArray);
+  stringArray[firstIndex] = stringArray[secondIndex];
+  console.log('stringArray[firstIndex]:', stringArray[firstIndex]);
+  console.log('newSecondIndex:', newSecondIndex);
+} */
+/* function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  var stringArray = string.split('');
+  var tempFirstIndex = stringArray[firstIndex];
+  console.log('tempFirstIndex', tempFirstIndex);
+  stringArray[firstIndex] = stringArray[secondIndex];
+  console.log('stringArray[firstIndex]:', stringArray[firstIndex]);
+  stringArray[secondIndex] = tempFirstIndex;
+  console.log('stringArray[secondIndex]:', stringArray[secondIndex]);
+  swappedCharacterString = stringArray.join('');
+  return swappedCharacterString;
+} */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharacterString = '';
+  var stringArray = string.split('');
+  var tempFirstIndex = stringArray[firstIndex];
+  stringArray[firstIndex] = stringArray[secondIndex];
+  stringArray[secondIndex] = tempFirstIndex;
+  swappedCharacterString = stringArray.join('');
   return swappedCharacterString;
 }
