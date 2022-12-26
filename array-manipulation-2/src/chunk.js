b04c13dd5f17710a075b340bb36bf636064bff90
Matt Cha <mcha002@ucr.edu>
2022-12-26 05:26:38 +0000

    array-manipulation-2  working 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,21 +2,12 @@
 /* -create a function with 2 arguments
 -create a empty array that will contain the array of arrays
 -need to create one main array that returns  */
-/* function chunk(array, size) {
-  var newArray = array.split('', 0, size);
-  console.log(newArray);
-  for (var i = 0; i < size; i++) {
+
+function chunk(array, size) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
     newArray.push(array[i]);
   }
-  return newArray.join('');
-} */
-/* need to use slice? */
-/* function chunk(array, size) {
-  var firstArray = array.slice(0, size);
-  var secondArray = array.slice(size, array.length - 1);
-  var finalArray =
-  console.log('firstArray:', firstArray);
 
-  return firstArray;
+  return newArray;
 }
- */
