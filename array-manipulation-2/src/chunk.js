ec0fe2bbfcbce43d954252ee5414041749feba1a
Matt Cha <mcha002@ucr.edu>
2022-12-27 05:27:33 +0000

    array-manipulation-2 finished

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,16 +1,18 @@
 /* exported chunk */
 /* -create a function with 2 arguments
 -create a empty array that will contain the array of arrays
--need to create one main array that returns  */
+-create a for loop that stops when the array length has been reached so
+that all elements in the array have been looped through
+-the final expression will add i to size and return the result back
+so that each sub array has that number of items in the array
+-slice the original array starting at index i and end the slice at
+i + size since we want to make the array length equal to size
+-push that sliced array into the new array  */
 
 function chunk(array, size) {
   var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    /*     newArray.push(array.slice([i])); */
-    array.slice(0);
-    /*     console.log(newArray.push(array.slice(0, 1))); */
-
+  for (var i = 0; i < array.length; i += size) {
+    newArray.push(array.slice(i, i + size));
   }
-
   return newArray;
 }
