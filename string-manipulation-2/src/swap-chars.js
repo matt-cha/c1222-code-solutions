4d2535e61ae9073c2859b4fddff438c293ffa6c6
Matt Cha <mcha002@ucr.edu>
2022-12-27 03:10:41 +0000

    string-manipulation-2 done

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,17 +1,19 @@
 /* exported swapChars */
 /* -create a function that takes 3 arguments
 -create a new empty string
--create a for loop that returns string secondIndex at the place of firstIndex
-and vice versa. to return secondIndex at the firstIndex,
--return rest of the string as normal
+-create a new array that takes the string and splits it by each character
+-set a new variable temp first index equal to the array at first Index argument
+-set the array at first index number = to the array at the second index number
+-set the array at second index number to the temporary first index variable (the array at first index)
+-let the new empty string equal the joined array by each character.
  */
 
 function swapChars(firstIndex, secondIndex, string) {
   var swappedCharacterString = '';
-  var stringArray = string.split('');
-  var tempFirstIndex = stringArray[firstIndex];
-  stringArray[firstIndex] = stringArray[secondIndex];
-  stringArray[secondIndex] = tempFirstIndex;
-  swappedCharacterString = stringArray.join('');
+  var array = string.split('');
+  var tempFirstIndex = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = tempFirstIndex;
+  swappedCharacterString = array.join('');
   return swappedCharacterString;
 }
