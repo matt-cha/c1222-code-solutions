f501917fdf73903f6e63baac221ba9bef004c5d5
Matt Cha <mcha002@ucr.edu>
2022-12-30 17:49:30 +0000

    array-manipulation-3 no change

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -4,10 +4,16 @@
 -create a for loop that stops at the array length
 -push the array at index of i into the new array
  */
-function flatten(array) {
+/* function flatten(array) {
   var flattenedArray = [];
   for (var i = 0; i < array.length; i++) {
     flattenedArray.push(array[i]);
   }
   return flattenedArray;
+} */
+/* function flatten(array) {
+  var flattenedArray = [].concat.apply([], array);
+  return flattenedArray;
 }
+need to figure outhow to do this with the isArray method DO NOT SUBMIT!
+ */
