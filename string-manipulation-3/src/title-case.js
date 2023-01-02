453eecef8fe7a30e0ef5813bd355b92076659595
Matt Cha <mcha002@ucr.edu>
2023-01-02 00:46:24 +0000

    string-manipulation-3 done

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,17 +1,30 @@
 /* exported titleCase */
 /* -create a function that takes 1 argument
+-create an array that will hold all prepositions, conjunctions
 -split the string into an array by a space character
--capitalize the first word
--if string's word length is greater than 4, capitalize
-special case: if string word is JavaScript or API
+-create a loop that goes through each element of the array,
+-at each element of the array, capitalize the first character and lowercase the rest of it with slice to return every character after the first
+-if the word ia exactly a lowercase javascript, return JavaScript instead.
+-if the word in uppercase at each element of the array is API, return API exactly for that index
+-run another loop that goes through the array and if any of the elements of the original argument's array are equal to an element in the array of conjunctions, return it in lowercase
+-if the element of the array is the first word of the entire argument, return it as uppercased first character.
+-join the array back together by a space argument
+-split the string by a colon and space character and assign to a new variable
+-if the array has more than 1 element in it, meaning there is a colon space character in the array,
+at the 1st index ( the second element in the array which is after the ': '), change the first character of that element to uppercase and return the rest of the element back with slice
+-join the array again by a colon space argument
+-split the string by a dash character
+-if that new array contains more than 1 element in the array, there is a - character in the argument so,
+at the 1st index, the element after the dash character, uppercase the first character and return the rest of the element with slice
+-join the element with a dash character into a new string
+
 - */
 
 function titleCase(string) {
   var array = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
-  var splitTitle = string.split(' ');
 
+  var splitTitle = string.split(' ');
   for (var i = 0; i < splitTitle.length; i++) {
-    /*     console.log(splitTitle[i]); */
     splitTitle[i] = splitTitle[i][0].toUpperCase() + splitTitle[i].slice(1).toLowerCase();
     if (splitTitle[i].toLowerCase() === 'javascript') {
       splitTitle[i] = splitTitle[i][0].toUpperCase() + splitTitle[i].slice(1, 4).toLowerCase() + splitTitle[i][4].toUpperCase() + splitTitle[i].slice(5).toLowerCase();
@@ -20,9 +33,7 @@ function titleCase(string) {
       splitTitle[i] = splitTitle[i].toUpperCase();
     }
     for (var k = 0; k < array.length; k++) {
-      if (splitTitle[i - 1].includes(':')) {
-        splitTitle[i] = splitTitle[i][0].toUpperCase() + splitTitle[i].slice(1).toLowerCase();
-      } else if (splitTitle[i].toLowerCase() === array[k]) {
+      if (splitTitle[i].toLowerCase() === array[k]) {
         splitTitle[i] = splitTitle[i].toLowerCase();
       }
     }
@@ -31,5 +42,20 @@ function titleCase(string) {
     }
   }
   var finalTitle = splitTitle.join(' ');
-  return finalTitle;
+  var colonSpaceTitle = finalTitle.split(': ');
+  if (colonSpaceTitle.length > 1) {
+    colonSpaceTitle[1] = colonSpaceTitle[1][0].toUpperCase() + colonSpaceTitle[1].slice(1);
+    var colonSpaceJoinedTitle = colonSpaceTitle.join(': ');
+  } else {
+    colonSpaceJoinedTitle = colonSpaceTitle.join(': ');
+  }
+  var colonSpaceFinalTitle = colonSpaceJoinedTitle.replace('Javascript', 'JavaScript');
+  var dashTitle = colonSpaceFinalTitle.split('-');
+  if (dashTitle.length > 1) {
+    dashTitle[1] = dashTitle[1][0].toUpperCase() + dashTitle[1].slice(1);
+    var apaTitle = dashTitle.join('-');
+  } else {
+    apaTitle = dashTitle.join('-');
+  }
+  return apaTitle;
 }
